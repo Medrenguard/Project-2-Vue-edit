19c7713a75ca556c03425d8f182aa6e63f8ebf1a
Nick <0892364395@mail.ru>
2021-11-09 00:32:51 +0300

    Обновлено левое меню на Vue

--- main.js
+++ main.js
@@ -4,5 +4,19 @@ var glossary = new Vue({
         currentTerm: undefined,
         currentItem: undefined,
         DB: outerDB,
+    },
+    computed:{
+        leftMenu: function () {
+            if (this.currentTerm === undefined) {
+                return 'Выберите категорию';
+            } else {
+                res = '<div class="sub-title">' + this.DB[this.currentTerm]["name"] + '</div><ul>';
+                for (i in this.DB[this.currentTerm]["data"]) {
+                    res += '<li><a class="nv">' + this.DB[this.currentTerm]["data"][i][1] + '</li>';
+                }
+                res += '</ul>';
+                return res;
+            }
+        }
     }
 })
