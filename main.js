c5ab40b7fe027c6aaf78e2d9209afcbf683eebbc
Nick <0892364395@mail.ru>
2021-11-11 00:28:02 +0300

    HTML+JS: в leftMenu заменен механизм генерации пунктов меню с html-кода внутри computed-функции на вывод директивой v-for

--- main.js
+++ main.js
@@ -6,17 +6,15 @@ var glossary = new Vue({
         DB: outerDB,
     },
     computed:{
-        leftMenu: function () {
+        termIsSet: function() {
+            return this.currentTerm != undefined;
+        },
+        itemsList: function() {
             if (this.currentTerm === undefined) {
-                return 'Выберите категорию';
+                return 0;
             } else {
-                res = '<div class="sub-title">' + this.DB[this.currentTerm]["nameRu"] + '</div><ul>';
-                for (i in this.DB[this.currentTerm]["data"]) {
-                    res += '<li><a class="nv">' + this.DB[this.currentTerm]["data"][i][1] + '</li>';
-                }
-                res += '</ul>';
-                return res;
+                return this.DB[this.currentTerm]["data"];
             }
-        }
+        },
     }
 })
