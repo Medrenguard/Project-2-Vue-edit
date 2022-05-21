b83af307c6b4660dd8da740476cd320658f6e6b0
Medrenguard <0892364395@mail.ru>
2022-05-21 22:24:19 +0300

    Добавление картинок и отображения текста

--- main.js
+++ main.js
@@ -19,5 +19,20 @@ var glossary = new Vue({
         itemIsSet: function() {
             return this.currentItem != undefined;
         },
+        content: function() {
+            return this.DB[this.currentTerm]["data"][this.currentItem][4];
+        },
+        imgDescription: function() {
+            return this.DB[this.currentTerm]["data"][this.currentItem][5];
+        },
+        getImgSrc: function() {
+            return 'img/content/' + this.DB[this.currentTerm]["data"][this.currentItem][0] + '-m.jpg';
+        },
+        itemName: function() {
+            return this.DB[this.currentTerm]["data"][this.currentItem][2];
+        },
+        bracketsDescription: function() {
+            return this.DB[this.currentTerm]["data"][this.currentItem][3];
+        },
     }
 })
