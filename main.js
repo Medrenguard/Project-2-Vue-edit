5fb51e67530225f53b470b21992dd5b73cb4da07
Medrenguard <0892364395@mail.ru>
2022-06-16 22:59:00 +0300

    Добавлено кеширование контента при переключении вкладок

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var glossary = new Vue({
     el: "#app",
     data: {
         currentTerm: undefined,
+        oldTerm: undefined,
         currentItem: undefined,
+        oldItem: undefined,
         DB: outerDB,
     },
     computed:{
@@ -17,22 +19,42 @@ var glossary = new Vue({
             }
         },
         itemIsSet: function() {
-            return this.currentItem != undefined;
+            return this.getCachedOptions.item != undefined;
         },
         content: function() {
-            return this.DB[this.currentTerm]["data"][this.currentItem][4];
+            return this.DB[this.getCachedOptions.term]["data"][this.getCachedOptions.item][4];
         },
         imgDescription: function() {
-            return this.DB[this.currentTerm]["data"][this.currentItem][5];
+            return this.DB[this.getCachedOptions.term]["data"][this.getCachedOptions.item][5];
         },
         getImgSrc: function() {
-            return 'img/content/' + this.DB[this.currentTerm]["data"][this.currentItem][0] + '-m.jpg';
+            return 'img/content/' + this.DB[this.getCachedOptions.term]["data"][this.getCachedOptions.item][0] + '-m.jpg';
         },
         itemName: function() {
-            return this.DB[this.currentTerm]["data"][this.currentItem][2];
+            return this.DB[this.getCachedOptions.term]["data"][this.getCachedOptions.item][2];
         },
         bracketsDescription: function() {
-            return this.DB[this.currentTerm]["data"][this.currentItem][3];
+            return this.DB[this.getCachedOptions.term]["data"][this.getCachedOptions.item][3];
         },
+        getCachedOptions: function() {
+            let res = {term: undefined, item: undefined}
+            if (this.currentTerm !== undefined && this.currentItem !== undefined) 
+                { res.term = this.currentTerm; res.item = this.currentItem }
+                else { res.term = this.oldTerm; res.item = this.oldItem }
+            return res;
+        }
+    },
+    methods:{
+        changeTerm(newTerm) {
+            if (this.currentTerm && this.currentItem !== undefined) {
+                this.oldItem = this.currentItem;
+                this.oldTerm = this.currentTerm; 
+                this.currentItem = undefined; 
+            }
+            this.currentTerm = newTerm;
+        },
+        changeItem(newItem) {
+            this.currentItem = newItem;
+        }
     }
 })
